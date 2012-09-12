5325bcd96a361d3317755857ee137dbd34420a4c
Phillip Cao <ftechz@gmail.com>
2012-09-12 16:26:37 +1200

    Added BtAutomationService and some initial BT handling code

---
+++ src/com/ftechz/tools/BtAutomationService.java
@@ -0,0 +1,58 @@
+package com.ftechz.tools;
+
+import android.app.Service;
+import android.bluetooth.BluetoothAdapter;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.IBinder;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: root
+ * Date: 12/09/12
+ * Time: 3:52 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class BtAutomationService extends Service {
+
+    BluetoothAdapter mBluetoothAdapter;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        if (mBluetoothAdapter == null) {
+            return;
+        }
+
+        if (mBluetoothAdapter.isEnabled()) {
+
+        }
+
+
+        registerReceiver(btActionReceiver,
+                new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED));
+
+
+    }
+
+    private BroadcastReceiver btActionReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getAction().equals(
+                    BluetoothAdapter.ACTION_STATE_CHANGED)) {
+
+                intent.getExtras().get(BluetoothAdapter.EXTRA_STATE);
+                
+            }
+        }
+    };
+
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
